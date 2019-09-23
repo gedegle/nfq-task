ba8dd1cdb3c09525a49fc3ffccd1608bea82e6e4
eglged <eglged@users.noreply.github.com>
2019-09-23 23:02:03 +0300

    Updates

---
+++ precache-manifest.5df4f541a0a222a0f4b6ca6b48c7860c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ef3a9289cbe3821051667348c1b0c72b",
+    "url": "/nfq-task/index.html"
+  },
+  {
+    "revision": "d167ac9a00752170e661",
+    "url": "/nfq-task/static/css/main.a4214397.chunk.css"
+  },
+  {
+    "revision": "244c1dc5c4d4a8cc9724",
+    "url": "/nfq-task/static/js/2.4f84b369.chunk.js"
+  },
+  {
+    "revision": "d167ac9a00752170e661",
+    "url": "/nfq-task/static/js/main.bc08032a.chunk.js"
+  },
+  {
+    "revision": "90963e9357f55d61c138",
+    "url": "/nfq-task/static/js/runtime~main.07ad4894.js"
+  },
+  {
+    "revision": "ee7cd8ed2dcec943251eb2763684fc6f",
+    "url": "/nfq-task/static/media/logo.ee7cd8ed.svg"
+  }
+]);
---
+++ spec-types.js
@@ -0,0 +1,42 @@
+window.SpecDirectory = {
+    "specTypes" : [
+        {
+            "key": "odontologas",
+            "display": "Odontologas"
+        },
+        {
+            "key":
+                "neurologas",
+            "display":
+                "Neurologas"
+        }
+        ,
+        {
+            "key":
+                "kardiologas",
+            "display":
+                "Kardiologas"
+        }
+        ,
+        {
+            "key":
+                "šeimos gydytojas",
+            "display":
+                "Šeimos gydytojas"
+        }
+        ,
+        {
+            "key":
+                "psichologas",
+            "display":
+                "Psichologas"
+        }
+        ,
+        {
+            "key":
+                "traumatologas",
+            "display":
+                "Traumatologas"
+        }
+    ]
+}
